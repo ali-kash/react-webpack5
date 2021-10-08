4070076f39d4925388b678b8b0cdf3eda636e4c4
Kai <ali.rf.kashani@gmail.com>
2021-10-08 13:00:31 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const mode = process.env.NODE_ENV || 'development'
+
+module.exports = {
+	// mode defaults to 'production' if not set
+	mode: mode,
+
+	// entry not required if using `src/index.js` default
+	// output not required if using `dist/main.js` default
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					// without additional settings, this will reference .babelrc
+					loader: 'babel-loader',
+				},
+			},
+		],
+	},
+
+	devtool: 'source-map',
+
+	// required if using webpack-dev-server
+	devServer: {
+		// contentBase: './dist',
+		static: path.join(__dirname, 'dist'),
+		port: 3001,
+	},
+}
